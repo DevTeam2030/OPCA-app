c3f2092c8a314e09f7e75ca753b9b3d36aaeb88e
Esraa Mohammad <esraamohammad107@gmail.com>
2024-08-16 09:33:20 +0300

    Update heading component

--- src/components/heading/HeadingText.jsx
+++ src/components/heading/HeadingText.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import "./heading-text.css";
+
 export default function HeadingText({ text }) {
+  const location = useLocation();
   const branchData = JSON.parse(localStorage.getItem("selectedBranch")) || {};
   const branchName = branchData.name || "";
+
   return (
     <div className="heading-wrapper">
-      <h4>{branchName}</h4>
+      {location.pathname !== "/order-complete" && <h4>{branchName}</h4>}
       <h3 className="heading-text">{text}</h3>
     </div>
   );
